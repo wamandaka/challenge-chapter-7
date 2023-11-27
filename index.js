13755bbdf61283e10404d9ea7de5c5a4382cc76b
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-27 08:13:00 +0700

    hapus kode tak terpakai dan tambah sentry

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use("/", router);
 
+const Sentry = require("@sentry/node");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
+
 app.get("/", (req, res) => {
   res.send("Hallo, Selamat datang!");
 });
