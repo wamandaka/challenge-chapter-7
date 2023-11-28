7eaa3e572dc136d9b2f4986a88f4cbd613f5143c
wamandaka <108261088+wamandaka@users.noreply.github.com>
2023-11-28 19:44:06 +0700

    add forgot and reset password

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -8,6 +8,14 @@ const {
   resetPassword,
 } = require("../controllers/auth_controller");
 
+router.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
 router.post("/register", register);
 
 router.post("/login", authUser);
